05cb1b5389ebf0d815550720fb99b3b88480030e
ManuLazcano <manu.lazcano17@gmail.com>
2024-09-27 17:46:42 -0300

    Added logic for SecondActivity

--- app/src/main/java/com/example/skycast/ForecastActivity.kt
+++ app/src/main/java/com/example/skycast/ForecastActivity.kt
@@ -1,15 +1,30 @@
 package com.example.skycast
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.skycast.adapter.ForecastAdapter
+import com.example.skycast.databinding.ActivityForecastBinding
+import com.example.skycast.model.ForecastDay
 
 class ForecastActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityForecastBinding
+    private lateinit var forecastAdapter: ForecastAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forecast)
+        binding = ActivityForecastBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        /// Obtener la lista pasada por el Intent
+        val forecastList = intent.getSerializableExtra("forecastList") as? List<ForecastDay> ?: emptyList()
+
+        initRecycler(forecastList)
+    }
+
+    private fun initRecycler(forecastList: List<ForecastDay>) {
+        binding.recyclerViewForecast.layoutManager = LinearLayoutManager(this)
+        forecastAdapter = ForecastAdapter(forecastList)
+        binding.recyclerViewForecast.adapter = forecastAdapter
     }
 }
